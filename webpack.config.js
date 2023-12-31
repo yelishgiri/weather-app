b5ec242805e72a6dce4df0715af8c92787a54230
Yelish Giri <118233081+GrapeJuice11111@users.noreply.github.com>
2023-12-31 09:57:43 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  }, // For more entry points
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // cleans directory when we build
+  },
+  plugins: [
+    // Add new HtmlWebpackPlugins for new entry points
+    new HtmlWebpackPlugin({
+      title: "",
+      template: "./src/index.html",
+      filename: "index.html", // specify name om html file
+      inject: "head", // injects js/ts files into the head,
+      scriptLoading: "defer",
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
